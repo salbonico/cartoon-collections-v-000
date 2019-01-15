c1b0feb076f81907e8b5ae0c8bc73792afed7245
Steve Albonico <salbonico@gmail.com>
2019-01-15 23:28:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   array.map do |planeteer|
-    planeteer.capitalize
+    planeteer= planeteer.capitalize
     planeteer+="!"
   end
 end
