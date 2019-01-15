144eac57f37127b05c8d82779bc6e6a81fb2b7f0
Steve Albonico <salbonico@gmail.com>
2019-01-15 23:24:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,8 @@ def roll_call_dwarves(array)# code an argument here
   i = 1
   array.each do |dwarf|
     puts "#{i}. #{dwarf}"
+    i+=1
+  end
 end
 
 def summon_captain_planet# code an argument here
