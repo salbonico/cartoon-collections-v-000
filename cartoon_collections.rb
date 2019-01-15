f742d229758ca337ea96b29293c256e636aac49d
Steve Albonico <salbonico@gmail.com>
2019-01-15 23:40:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,11 +18,12 @@ def long_planeteer_calls(array)# code an argument here
 end
 
 def find_the_cheese(array)# code an argument here
+cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     if array.include?(cheese)
       return cheese
     end
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
 return nil
 end
