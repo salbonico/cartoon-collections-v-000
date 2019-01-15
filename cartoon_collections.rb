c9ea04fb21f57099d9b8c53d690faa4cd6ec0cb7
Steve Albonico <salbonico@gmail.com>
2019-01-15 23:30:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.include?(array.length > 4)
 end
 
 def find_the_cheese# code an argument here
